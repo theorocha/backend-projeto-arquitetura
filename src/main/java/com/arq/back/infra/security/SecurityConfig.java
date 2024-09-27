90b195936f52a8737b5f2c5ffcb3824c1d10fae7
Theo Rocha <51010226+theorocha@users.noreply.github.com>
2024-09-26 22:59:11 -0300

    lint: sonar

--- src/main/java/com/arq/back/infra/security/SecurityConfig.java
+++ src/main/java/com/arq/back/infra/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -30,7 +31,7 @@ public class SecurityConfig {
                     return config;
                 }))
                 .headers(headers -> headers
-                        .frameOptions(frameOptions -> frameOptions.disable())
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
                         .addHeaderWriter(new StaticHeadersWriter("X-Frame-Options", "ALLOW-FROM http://localhost:8080"))
                 )
                 .authorizeHttpRequests(authorize -> authorize
--- src/main/java/com/arq/back/servicoextra/ServicoExtraResource.java
+++ src/main/java/com/arq/back/servicoextra/ServicoExtraResource.java
@@ -2,11 +2,8 @@ package com.arq.back.servicoextra;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/servico-extra")
